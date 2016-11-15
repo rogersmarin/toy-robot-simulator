73845eb7a54325b46bad282679ed2324cde048aa
Roger Marin <roger.marin@colesfinancialservices.com.au>
2016-11-15 00:49:49 -0500

    Refactoring CommandParserSpec

--- src/test/scala/com/rogersmarin/toyrobot/CommandParserSpec.scala
+++ src/test/scala/com/rogersmarin/toyrobot/CommandParserSpec.scala
@@ -20,40 +20,14 @@ class CommandParserSpec extends WordSpec with Matchers{
     }
 
     "Throw an InvalidCommandException when attempting to parse invalid commands" in {
-      var thrown = intercept[InvalidCommandException] { parser.parse("MOVE LEFT") }
-      assert(thrown.message === "Unable to parse command: MOVE LEFT")
-      thrown = intercept[InvalidCommandException] { parser.parse("MOVE RIGHT") }
-      assert(thrown.message === "Unable to parse command: MOVE RIGHT")
-      thrown = intercept[InvalidCommandException] { parser.parse("STOP") }
-      assert(thrown.message === "Unable to parse command: STOP")
-      thrown = intercept[InvalidCommandException] { parser.parse("GO") }
-      assert(thrown.message === "Unable to parse command: GO")
-      thrown = intercept[InvalidCommandException] { parser.parse("move") }
-      assert(thrown.message === "Unable to parse command: move")
-      thrown = intercept[InvalidCommandException] { parser.parse("left") }
-      assert(thrown.message === "Unable to parse command: left")
-      thrown = intercept[InvalidCommandException] { parser.parse("right") }
-      assert(thrown.message === "Unable to parse command: right")
-      thrown = intercept[InvalidCommandException] { parser.parse("report") }
-      assert(thrown.message === "Unable to parse command: report")
-      thrown = intercept[InvalidCommandException] { parser.parse("PLACE") }
-      assert(thrown.message === "Unable to parse command: PLACE")
-      thrown = intercept[InvalidCommandException] { parser.parse("PLACE 2") }
-      assert(thrown.message === "Unable to parse command: PLACE 2")
-      thrown = intercept[InvalidCommandException] { parser.parse("PLACE SOUTHWEST") }
-      assert(thrown.message === "Unable to parse command: PLACE SOUTHWEST")
-      thrown = intercept[InvalidCommandException] { parser.parse("PLACE SOUTH") }
-      assert(thrown.message === "Unable to parse command: PLACE SOUTH")
-      thrown = intercept[InvalidCommandException] { parser.parse("PLACE 2,SOUTH") }
-      assert(thrown.message === "Unable to parse command: PLACE 2,SOUTH")
-      thrown = intercept[InvalidCommandException] { parser.parse("PLACE 2,SOUTHWEST") }
-      assert(thrown.message === "Unable to parse command: PLACE 2,SOUTHWEST")
-      thrown = intercept[InvalidCommandException] { parser.parse("PLACE 2,2,SOUTHWEST") }
-      assert(thrown.message === "Unable to parse command: PLACE 2,2,SOUTHWEST")
-      thrown = intercept[InvalidCommandException] { parser.parse("place 2,2,NORTH") }
-      assert(thrown.message === "Unable to parse command: place 2,2,NORTH")
-      thrown = intercept[InvalidCommandException] { parser.parse("") }
-      assert(thrown.message === "Unable to parse command: ")
+      val invalidCommands = List("MOVE LEFT","MOVE RIGHT","STOP", "GO", "move",
+                                 "left", "right", "report", "PLACE", "PLACE 2",
+                                 "PLACE SOUTHWEST", "PLACE SOUTH", "PLACE 2,SOUTH", "PLACE 2,SOUTHWEST",
+                                 "PLACE 2,2,SOUTHWEST", "place 2,2,NORTH", "")
+      invalidCommands.foreach(command=> {
+        val thrown = intercept[InvalidCommandException] { parser.parse(command) }
+        assert(thrown.message === s"Unable to parse command: ${command}")
+      })
     }
   }
 }
